b611061f98d148d99187b8e1ef65f8fab5ad32cf
Kai McCloud <aymeealvarezv@gmail.com>
2022-11-01 21:15:48 -0600

    Ingresar correctamente
    Arreglé errores porque no podía ingresar

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -92,7 +92,7 @@ const Input = () => {
         <label htmlFor="file">
           <img src={Img} alt="" />
         </label>
-        <button onClick={handleSend}>Send</button>
+        <button onClick={handleSend}>Enviar</button>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,85 +1,88 @@
-import React, { useState } from 'react'
-import Add from "../img/addAvatar.png"
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import {auth, db,  storage} from "../firebase";
-import {ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore"; 
-import { useNavigate, Link } from "react-router-dom"
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
-    const [err, setErr] = useState (false);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
-    const displayName= e.target[0].value;
-    const email= e.target[1].value;
-    const password= e.target[2].value;
-    const file= e.target[3].files[0];
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
     try {
-        //Create user
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-  
-        //Create a unique image name
-        const date = new Date().getTime();
-        const storageRef = ref(storage, `${displayName + date}`);
-  
-        await uploadBytesResumable(storageRef, file).then(() => {
-          getDownloadURL(storageRef).then(async (downloadURL) => {
-            try {
-              //Update profile
-              await updateProfile(res.user, {
-                displayName,
-                photoURL: downloadURL,
-              });
-              //create user on firestore
-              await setDoc(doc(db, "users", res.user.uid), {
-                uid: res.user.uid,
-                displayName,
-                email,
-                photoURL: downloadURL,
-              });
-  
-              //create empty user chats on firestore
-              await setDoc(doc(db, "userChats", res.user.uid), {});
-              navigate("/");
-            } catch (err) {
-              console.log(err);
-              setErr(true);
-              setLoading(false);
-            }
-          });
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
         });
-      } catch (err) {
-        setErr(true);
-        setLoading(false);
-      }
+      });
+    } catch (err) {
+      setErr(true);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="Nombre" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="Contraseña" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Agrega una imagen</span>
+          </label>
+          <button disabled={loading}>Regístrate aquí</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Algo salió mal</span>}
+        </form>
+        <p>
+          ¿Tienes una cuenta? <Link to="/register">Inicia sesión</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
-    return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">VACChatApp</span>
-                <span className="title">Registrarse</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Nombre"/>
-                    <input type="email" placeholder="email"/>
-                    <input type="password" placeholder="Contraseña"/>
-                    <input style={{display: "none"}} type="file"  id="file"/>
-                    <label htmlFor="file" >
-                        <img src={Add} alt="" />
-                        <span>Agregar foto de perfil</span>
-                         </label>
-                    <button>Registrarse</button>
-                    {err && <span>Algo salió mal</span>}
-                    <p>¿Ya tienes una cuenta? Inicia sesión</p>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default Register 
+export default Register;
