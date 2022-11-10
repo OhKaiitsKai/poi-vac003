0bf5bb0179ed1ecf523deff02a2074513beb8365
Maxter-San <102878819+Maxter-San@users.noreply.github.com>
2022-11-09 18:58:04 -0600

    a

--- src/components/Createchatroom.jsx
+++ src/components/Createchatroom.jsx
@@ -2,25 +2,25 @@ import React, {useEffect, useState} from 'react';
 import Createroom from '../img/room.png';
 //import { AuthContext } from "../context/AuthContext";
 //import { ChatContext } from "../context/ChatContext";
-//import { doc, setDoc } from "firebase/firestore";
-//import {db} from './firebase';
+import { doc, setDoc } from "firebase/firestore";
+import {db} from '../firebase';
 //import {Link} from 'react-router-dom';
 
 const Createchatroom = () => {
-  //const [messages, setMessages] = useState([]);
-  //const { data } = useContext(ChatContext);
-  //const { currentUser } = useContext(AuthContext);
-  //const { dispatch } = useContext(ChatContext);
+const [croom, setCroom]= useState([]);
 
-  const roomName = prompt("Por favor, ingrese un nombre para la sala:");
+  const roomName=()=>{
+    const setRoom = prompt("Por favor, ingrese un nombre para la sala:");
+    setCroom(setRoom);
+    if (setRoom){
+    
+    }
+  } 
 
-  //      if(roomName){
-    //        db.collection("rooms").add({
-      //          name: roomName
-        //    })
-        //}
+
+
     return (
-        <div className='chatroom'>
+        <div onClick={roomName}className='chatroom'>
          <img src={Createroom} alt=''/> 
          <div className='roomtxt'>
          <span>Crear sala</span>
