201175052e2d1e290b20956ed5bab27685336121
Kai McCloud <104228712+OhKaiitsKai@users.noreply.github.com>
2022-11-22 05:46:13 +0000

    Creación de salas.

--- src/components/Createchatroom.jsx
+++ src/components/Createchatroom.jsx
@@ -1,24 +1,38 @@
 import React, {useContext, useState} from 'react';
 import Createroom from '../img/room.png';
-//import { AuthContext } from "../context/AuthContext";
-import { ChatContext } from "../context/ChatContext";
-import { doc, setDoc, Timestamp } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
+//import { ChatContext } from "../context/ChatContext";
+import { doc, setDoc, updateDoc, Timestamp, serverTimestamp } from "firebase/firestore";
 import {db} from '../firebase';
 import { v4 as uuid } from "uuid";
 //import {Link} from 'react-router-dom';
 
 const Createchatroom = () => {
-const { data } = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+  //const { data } = useContext(ChatContext);
 
   const roomName= async()=>{
     const setRoom = prompt("Por favor, ingrese un nombre para la sala:");
     if (setRoom){
       //Create new room
-      await setDoc(doc(db, "Room", data.chatId), {
-        id: uuid(),
-        newroom: setRoom,
+      const roomid= uuid();
+      await setDoc(doc(db, "Room", roomid), {
+        id: roomid,
+        nameroom: setRoom,
         Creationdate: Timestamp.now(),
+        members: [],
       });
+      //create a chat in chats collection
+      await setDoc(doc(db, "chats", roomid), { messages: [] });
+      //create user chats
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [roomid+ ".userInfo"]: {
+          nameroom: setRoom,
+          Creationdate: Timestamp.now(),
+        },
+        [roomid + ".date"]: serverTimestamp(),
+      });
+
     }
   } 
 
