ac75c4f14d04af1c5365aaa116b700e3d9b53651
Kai McCloud <104228712+OhKaiitsKai@users.noreply.github.com>
2022-12-01 05:12:31 +0000

    Falta ubicación y encriptación.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,33 @@
 import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
+//import CryptoJS from "crypto-js";
 
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
+
+  //const [enc, setEnc]= useState(false);
 
   const ref = useRef();
 
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
+
+  //const encdec = async() => { 
+  //if(enc){
+    //var ciphertext = CryptoJS.AES.encrypt(message.text, 'miClave').toString();
+    //setEnc(true);
+    //return ciphertext;
+//}
+  //else{
+    //var bytes = CryptoJS.AES.decrypt(message.text, 'miClave');
+    //var decryptedData =bytes.toString(CryptoJS.enc.Utf8);
+    //setEnc(false);
+    //return decryptedData;
+  //}
+//}
 
   return (
     <div
